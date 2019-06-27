201acfbc89936fc45b174be1a98342990e1cf797
Brian <briandavis1977@gmail.com>
2019-06-26 21:18:28 -0400

    removing array index from learn.predict call

--- app/server.py
+++ app/server.py
@@ -62,7 +62,7 @@ async def analyze(request):
     #img_data = await request.form()
     #img_bytes = await (img_data['file'].read())
     #img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(comment)[0]
+    prediction = learn.predict(comment)
     return JSONResponse({'result': str(prediction)})
 
 
